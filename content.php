07222516a21b39df63cdbad6a23fe96fb832bea9
erafas <91721271+erafas@users.noreply.github.com>
2021-10-01 10:49:43 +0330

    Add files via upload

---
+++ content.php
@@ -0,0 +1,14 @@
+<div class='wire-card'>
+        <div>
+            <img src='assets/image/device-animate.jpg' class='img-emo'>
+            <p class='txt-emo'>Twice than .1</p>
+        </div>
+        <div class='dv-m'>
+            <img src='assets/image/toto-animate.jpg' class='img-emo'>
+            <p class='txt-emo'>Twice than .2</p>
+        </div>
+        <div>
+            <img src='assets/image/work-animate.jpg' class='img-emo'>
+            <p class='txt-emo'>Twice than .3</p>
+        </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,18 @@
+<div class='fotr-card'>
+    <div class='fotr-ci'>
+        <div>
+            <p class='col-txt'>Quick Links</p>
+            <a href='#' class='lnkftr'>Contact</a>
+        </div>
+        <div class='cl-rf'> 
+            <p class='col-txt'>Support</p>
+            <a href='#' class='lnkftr'>Privacy Policy</a>
+        </div>
+        <div>
+            <p class='col-txt'>Core Features</p>
+            <a href='#' class='lnkftr'>24/7 Support</a>
+        </div>
+    </div>
+    <img src='assets/image/wall-footer.jpg' class='fotr-wall'>
+    <span class='fotr-cpy'>©2020 create by <a href='https://www.vearta.com' id='lnkcope'>vearta.com</a></span>
+</div>
